59fce4999c735b6f8366359a55ca0c8880e0a4f9
Tyler Jachetta <me@tylerjachetta.net>
2017-12-02 15:02:30 -0500

    adding some docstrings, improving PEP8 compliance

--- day01/01/solution.py
+++ day01/01/solution.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python
 
 """
---- Part Two ---
-
-You notice a progress bar that jumps to 50% completion. Apparently, the door
-isn't yet satisfied, but it did emit a star as encouragement. The
-instructions change:
-
-Now, instead of considering the next digit, it wants you to consider the digit
-halfway around the circular list. That is, if your list contains 10 items,
-only include a digit in your sum if the digit 10/2 = 5 steps forward matches
-it. Fortunately, your list has an even number of elements.
-
-For example:
-
-1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
-1221 produces 0, because every comparison is between a 1 and a 2.
-123425 produces 4, because both 2s match each other, but no other digit has a match.
-123123 produces 12.
-12131415 produces 4.
+Solution script for the secont problem on day 1 of AOC.
 """
 
 import sys
 
+
 def test():
+    """
+    Test my solution with examples from the AOC problem description.
+    """
+
     assert run('1212') == 6
     assert run('1221') == 0
     assert run('123425') == 4
     assert run('123123') == 12
     assert run('12131415') == 4
+
 
 def run(in_val):
     digits = [int(i) for i in in_val.strip()]
@@ -36,7 +25,7 @@ def run(in_val):
     sum_val = 0
 
     for idx in range(len(digits)):
-        if digits[idx] == digits[idx-(len(in_val)/2)]:
+        if digits[idx] == digits[idx - (len(in_val) / 2)]:
             sum_val += digits[idx]
     return sum_val
 
