ed296e5c88a7c1d49d95963975cef42ec228a7b3
Tyler Jachetta <me@tylerjachetta.net>
2017-12-02 15:02:47 -0500

    adding some docstrings, improving PEP8 compliance

--- day02/01/solution.py
+++ day02/01/solution.py
@@ -1,48 +1,28 @@
 #!/usr/bin/env python
 
 """
---- Part Two ---
-
-"Great work; looks like we're on the right track after all. Here's a star for
-your effort." However, the program seems a little worried. Can programs be
-worried?
-
-"Based on what we're seeing, it looks like all the User wanted is some
-information about the evenly divisible values in the spreadsheet.
-Unfortunately, none of us are equipped for that kind of calculation - most of
-us specialize in bitwise operations."
-
-It sounds like the goal is to find the only two numbers in each row where one
-evenly divides the other - that is, where the result of the division operation
-is a whole number. They would like you to find those numbers on each line,
-divide them, and add up each line's result.
-
-For example, given the following spreadsheet:
-
-5 9 2 8
-9 4 7 3
-3 8 6 5
-In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
-In the second row, the two numbers are 9 and 3; the result is 3.
-In the third row, the result is 2.
-In this example, the sum of the results would be 4 + 3 + 2 = 9.
-
+Solution script for the second day 2 problem.
 """
-
 
 import sys
 
-
-def test():
-    test_val = """5 9 2 8
+_TEST_VAL = """5 9 2 8
 9 4 7 3
 3 8 6 5"""
-    assert run(test_val) == 9
+
+
+def test():
+    assert run(_TEST_VAL) == 9
 
 
 def run(input_val):
+    """
+    Main body for solution
+    """
+
     checksum = 0
     for row in input_val.splitlines():
+        # Sort the rows, so we can save loops.
         row_vals = sorted([int(v) for v in row.split()], reverse=True)
 
         while row_vals:
@@ -51,9 +31,11 @@ def run(input_val):
                 if not a % b:
                     break
             else:
+                # If we haven't found an even division, move to the
+                # next numerator.
                 continue
 
-            checksum += a/b
+            checksum += a / b
 
     return checksum
 
