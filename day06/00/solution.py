f981e68c8af8f2ddb56279a03864eb7e7c9602aa
Tyler Jachetta <me@tylerjachetta.net>
2017-12-11 19:07:58 -0700

    final day 6 work

--- day06/00/solution.py
+++ day06/00/solution.py
@@ -1,4 +1,6 @@
 
+import util
+
 def test():
 
     assert run("0 2 7 0") == 5
@@ -6,16 +8,6 @@ def test():
 
 def run(in_val):
 
-    banks = [int(v) for v in in_val.split()]
-    seen = []
-
-    while banks not in seen:
-        seen.append(list(banks))
-        high_mem = max(banks)
-        highest_idx = banks.index(high_mem)
-        banks[highest_idx] = 0
-        for idx in range(high_mem):
-            bank_idx = (highest_idx + idx + 1) % len(banks)
-            banks[bank_idx] += 1
+    seen, _dupe_idx = util.get_cycles(in_val)
 
     return len(seen)
